f20304967ad0c92a35af34f49ad0494fb3e40a38
JorgeVictoria <jvandreu@hotmail.com>
2022-07-19 18:14:51 +0200

    inicio Curso

--- 001-HolaMundo/src/HolaMundo.java
+++
@@ -1,8 +0,0 @@
-public class HolaMundo {                            //inicio clase
-
-    public static void main(String[] args) {        //inicio main
-
-        System.out.println("Hola Mundo desde Java");
-
-    }                                               //fin main
-}                                                   //fin clase
